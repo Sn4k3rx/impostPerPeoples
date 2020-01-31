d2bd8a3588efff8565622d5418269941f37f0486
Sn4k3rx <ramonvieira122333@gmail.com>
2020-01-31 06:19:25 -0300

    Classe de pessoas físicas

--- src/entities/FisicPeoples.java
+++ src/entities/FisicPeoples.java
@@ -1,5 +1,34 @@
 package entities;
 
-public class FisicPeoples {
-
+public class FisicPeoples extends Peoples{
+	
+	private Double valueHospital;
+	
+	public FisicPeoples() {
+		super();
+	}
+	
+	public FisicPeoples(String name, Double valueYear, Double valueHospital) {
+		super(name, valueYear);
+		this.valueHospital = valueHospital;
+	}
+	
+	public Double getValueHospital() {
+		return valueHospital;
+	}
+	
+	public void setValueHospital(Double value) {
+		this.valueHospital = value;
+	}
+	
+	@Override
+	public Double impost() {
+		if (getValueYear() < 20000) {
+			return getValueYear() * 0.15 - valueHospital * 0.5;
+		}else {
+			return getValueYear() * 0.25 - valueHospital * 0.5;
+		}
+	}
+	
+	
 }

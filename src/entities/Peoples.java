3943ce73a7613a198ca74761ca96050016127687
Sn4k3rx <ramonvieira122333@gmail.com>
2020-01-31 05:59:38 -0300

    Criação de classes e implementação da classe abstrata

---
+++ src/entities/Peoples.java
@@ -0,0 +1,35 @@
+package entities;
+
+public abstract class Peoples {
+	
+	private String name;
+	private Double valueYear;
+	
+	public Peoples() {
+		
+	}
+	
+	public Peoples(String name, double valueYear) {
+		this.name = name;
+		this.valueYear = valueYear;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getValueYear() {
+		return valueYear;
+	}
+
+	public void setValueYear(Double valueYear) {
+		this.valueYear = valueYear;
+	}
+	
+	public abstract Double impost();
+	
+}
